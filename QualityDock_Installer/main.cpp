c2efde6e62e95c7e2b87edc7a56e1037a361d949
Julien <julien.haillecourt@gmail.com>
2015-12-05 17:43:48 +0100

    Default project for Installer
    Default project for installer

---
+++ QualityDock_Installer/main.cpp
@@ -0,0 +1,26 @@
+﻿#include "mainwindow.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+    return a.exec();
+}
+
+
+/*
+ * void MainWindow::changeEvent(QEvent *e)
+{
+    QMainWindow::changeEvent(e);
+    switch (e->type()) {
+    case QEvent::LanguageChange:
+        ui->retranslateUi(this);
+        break;
+    default:
+        break;
+    }
+}
+*/
